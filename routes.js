602dd4d49835ccaf33487d25e2171cef0b728746
Titouan LEFEVRE <titouan.lefevre@epitech.eu>
2024-04-13 15:02:46 +0200

    [MOD] Move routes.js into the main folder to use it and now use the correct css for the pages

--- routes.js
+++ routes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 
+// Get the css
+app.use('/css', express.static(__dirname + '/css'));
+
 // Route for dashboard.html
 app.get('/dashboard', (req, res) => {
     res.sendFile(__dirname + '/dashboard.html');
 });
 
 // Route for login_signup.html
-app.get('/login', (req, res) => {
+app.get('/login_signup', (req, res) => {
     res.sendFile(__dirname + '/login_signup.html');
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+// Listen to the specified port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
