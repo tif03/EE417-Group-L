821a9dc26cdf3f1d89b5e7b559b6c1471cc4864e
Tiffany Yu <tif03.yu@gmail.com>
2024-04-15 00:51:25 +0200

    slight front end modifications

--- webapp/target/classes/static/js/login_signup.js
+++ webapp/target/classes/static/js/login_signup.js
@@ -35,36 +35,14 @@ document.addEventListener('DOMContentLoaded', function(event) {
         submitRegisterForm();
     });
 
-    // Handle login
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
-        console.log("Login form submitted");
-
-        const formData = new FormData(loginForm);
-        const email = formData.get('email');
-        const password = formData.get('password');
-
-        fetch('/api/users/login', {  // Make sure to adjust the URL based on your API
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email, password })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                console.log('Login successful');
-                // Redirect to another page or update the UI as needed
-            } else {
-                console.error('Login failed: ' + data.message);
-                // Show error message to the user
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    });
+	 // Handle login
+	loginForm.addEventListener('submit', function(event) {
+	    event.preventDefault(); // Prevent the default form submission
+	    console.log("Login form submitted");
+	
+	    // Directly redirect to the dashboard without making an API call
+	    window.location.href = "dashboard.html";
+});
 
     // Ensure the body is visible after everything is loaded
     document.body.style.opacity = 1;
@@ -87,13 +65,14 @@ document.addEventListener('DOMContentLoaded', function(event) {
         })
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                 return response.text().then(text => { throw new Error(text) }); // Change here to throw text error
+
             }
             return response.json();
         })
         .then(data => {
             console.log('Success:', data);
-            window.location.href = "login.html";  
+            window.location.href = "dashboard.html";  
         })
         .catch((error) => {
             console.error('Error:', error);
