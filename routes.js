3b8b896773f7ea3039509617c05f8610bd773ac7
Titouan LEFEVRE <titouan.lefevre@epitech.eu>
2024-04-14 11:25:46 +0200

    [MOD] Add route for the new pages in routes.js

--- routes.js
+++ routes.js
@@ -14,6 +14,26 @@ app.get('/login_signup', (req, res) => {
     res.sendFile(__dirname + '/login_signup.html');
 });
 
+// Route for calendar.html
+app.get('/calendar', (req, res) => {
+    res.sendFile(__dirname + '/calendar.html');
+});
+
+// Route for map.html
+app.get('/map', (req, res) => {
+    res.sendFile(__dirname + '/map.html');
+});
+
+// Route for room.html
+app.get('/room', (req, res) => {
+    res.sendFile(__dirname + '/room.html');
+});
+
+// Route for weather.html
+app.get('/weather', (req, res) => {
+    res.sendFile(__dirname + '/weather.html');
+});
+
 // Listen to the specified port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
